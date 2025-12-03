91e165f9d7da3c657c4826b4f0d0855f0c0f4181
Pim van Leeuwen <mail@pvleeuwen.com>
2025-12-03 06:10:26 +0100

    Completed day 3

--- days/day03.py
+++ days/day03.py
@@ -2,4 +2,26 @@ from utils import *
 
 def solve(input_data: str):
     lines = read_lines(input_data)
-    return NotImplementedError("This day is not yet implemented")
+    parts = [0,0]
+
+    for line in lines:
+
+        # Part 1, brute force
+        digits = list(line.strip())
+        parts[0] += max(int(digits[i] + digits[j]) for i in range(len(digits)) for j in range(i + 1, len(digits)))
+
+        # Part 2, take the max number, from the available numbers, greedy approach
+        n = len(digits)
+        result = []
+        start = 0
+        for k in range(12, 0, -1):
+            # get the largest digit, index from the subarray digits[start:n - k + 1] (we still need to remainder for other digits)
+            largest = max(digits[start:n - k + 1])
+            i = digits.index(largest, start, n - k + 1)
+
+            # Append that digit
+            result.append(largest)
+            start = i + 1
+        parts[1] += int(''.join(result))
+
+    return parts
