be3c5e11c514e36e43ffbfba295d928e484e2fd4
Pim van Leeuwen <mail@pvleeuwen.com>
2025-12-04 06:20:48 +0100

    Completed day 4

--- days/day04.py
+++ days/day04.py
@@ -1,5 +1,26 @@
 from utils import *
+from itertools import product
 
 def solve(input_data: str):
-    lines = read_lines(input_data)
-    return NotImplementedError("This day is not yet implemented")
+    grid = [list(line) for line in input_data.split('\n')]
+    parts = [0,0]
+
+    # Part 1
+    for y, x in product(range(len(grid)), range(len(grid[0]))):
+        rolls = sum(1 for dy, dx in neighbors_8(y, x, len(grid), len(grid[0])) if grid[dy][dx] == '@')
+        if grid[y][x] == '@' and rolls < 4:
+            parts[0] += 1
+
+    # Part 2
+    while True:
+        changed = False
+        for y, x in product(range(len(grid)), range(len(grid[0]))):
+            rolls = sum(1 for dy, dx in neighbors_8(y, x, len(grid), len(grid[0])) if grid[dy][dx] == '@')
+            if grid[y][x] == '@' and rolls < 4:
+                grid[y][x] = '.'
+                parts[1] += 1
+                changed = True
+        if not changed:
+            break
+
+    return parts
