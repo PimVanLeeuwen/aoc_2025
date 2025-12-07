d229d57ef1e346e97d88b7c6104f7d8f6f1b8d6f
Pim van Leeuwen <mail@pvleeuwen.com>
2025-12-07 10:04:57 +0100

    Completed day 7, waaaay too slow :(

--- days/day07.py
+++ days/day07.py
@@ -1,5 +1,44 @@
-from utils import *
+from functools import lru_cache
+from collections import deque
 
 def solve(input_data: str):
-    lines = read_lines(input_data)
-    return NotImplementedError("This day is not yet implemented")
+    grid = [list(line) for line in input_data.split('\n')]
+    parts = [0,0]
+
+    q = deque()
+
+    q.append((0, 70))
+    visited = {(0, 70)}
+
+    splits = set()
+
+    while q:
+        y, x = q.popleft()
+
+        if (y + 1) < len(grid) and grid[y + 1][x] == '.':
+            visited.add((y + 1, x))
+            q.append((y + 1, x))
+        elif (y + 1) < len(grid) and grid[y + 1][x] == '^':
+            if not (y + 1, x - 1) in visited:
+                visited.add((y + 1, x - 1))
+                q.append((y + 1, x - 1))
+            if not (y + 1, x + 1) in visited:
+                visited.add((y + 1, x + 1))
+                q.append((y + 1, x + 1))
+            splits.add((y+1, x))
+
+    @lru_cache(maxsize=None)
+    def count_routes(y, x):
+        if y >= len(grid) or x < 0 or x >= len(grid[0]):
+            return 0
+        if y == len(grid) - 1:
+            return 1
+        if grid[y][x] == '^':
+            return count_routes(y + 1, x - 1) + count_routes(y + 1, x + 1)
+        else:
+            return count_routes(y + 1, x)
+
+    parts[0] = len(splits)
+    parts[1] = count_routes(0, 70)
+
+    return parts
