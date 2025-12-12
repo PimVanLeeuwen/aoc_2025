fd9082bf15523a6ee11c12438f7a1cb9601b7dfb
Pim van Leeuwen <mail@pvleeuwen.com>
2025-12-12 06:30:10 +0100

    Completed day 12

--- days/day12.py
+++ days/day12.py
@@ -1,5 +1,23 @@
-from utils import *
+from utils import read_lines
 
-def solve(input_data: str):
-    lines = read_lines(input_data)
-    return NotImplementedError("This day is not yet implemented")
+def solve(input_data):
+    parts = [0,0]
+
+    regions_part = read_lines(input_data.split("\n\n")[-1])
+    regions = []
+    for line in regions_part:
+        left, right = line.split(":")
+        w, h = map(int, left.split("x"))
+        counts = list(map(int, right.strip().split()))
+        regions.append((w, h, counts))
+
+    # naive check for part 1
+    for w, h, counts in regions:
+        total_presents = sum(counts)
+
+        capacity = (w // 3) * (h // 3)
+
+        if total_presents <= capacity:
+            parts[0] += 1
+
+    return parts
